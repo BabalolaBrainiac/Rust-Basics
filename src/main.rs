531104c35aed1bfc7c135cbb447851bdb61da1c5
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-09-01 14:55:08 +0100

    completed excercise

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,19 @@
-struct Product {
-    name: String,
-    price: f32,
-    in_stock: bool,
-}
+// Complete the structure definition.
+//Tuple Struct Implementation
+struct Point(f32, f32);
 
-impl Product {
-    fn new(name: String, price: f32) -> Product {
-        Product {
-            name: (name),
-            price: (price),
-            in_stock: (false),
-        }
+impl Point {
+    fn on_x_axis(&self) -> bool {
+        self.1 == 0.0
     }
-
-    fn get_default_sales_tax() -> f32 {
-        0.1
-    }
-
-    fn calculate_sales_tax(&self) -> f32 {
-        self.price * 0.1
-    }
-
-    fn set_price(&mut self, price: f32) {
-        self.price = price
-    }
-
-    fn buy(self) -> i32 {
-        let name = self.name;
-        println!("{name} was bought");
-        234
+    fn on_y_axis(&self) -> bool {
+        self.0 == 0.0
     }
 }
 
 fn main() {
-    let mut book = Product::new(String::from("Book"), 25.0);
-
-    // let price = book.price;
-    book.in_stock = false;
-
-    let sales_tax = book.calculate_sales_tax();
-
-    book.set_price(25.0);
-    book.buy();
-    // book.set_price(23.3);
-
-    println!("Sales tax = {}", sales_tax)
+    let point = Point(0.0, 0.0);
+    if point.on_x_axis() && point.on_y_axis() {
+        println!("Point is origin");
+    }
 }
