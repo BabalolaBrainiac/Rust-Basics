f8659dbb44d46ac6a356c82095bf32bdcf9ed460
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-09-01 15:15:38 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 // Enums
+//Example 01
+// struct Product {
+//     name: String,
+//     price: f32,
+//     in_stock: bool,
+//     category: ProductCategory,
+// }
+//
 
-struct Product {
-    name: String,
-    price: f32,
-    in_stock: bool,
-    category: ProductCategory,
-}
+//Example 02
+// enum ProductCategory {
+//     Clothing,
+//     Electronics,
+//     Books,
+// }
 
-enum ProductCategory {
-    Clothing,
-    Electronics,
-    Books,
+enum Commands {
+    AddText,
+    Undo,
+    Replace {
+        from: String,
+        to: String,
+    },
+    moveCursor ( i32, i32)
 }
 
 fn main() {
-    let category = ProductCategory::Electronics;
 
-    let newProduct = Product {
-        name: String::from("Book"),
-        price: 300.0,
-        in_stock: true,
-        category,
-    };
+
+
+
+    //Example 01
+    // let category = ProductCategory::Electronics;
+    //
+    // let newProduct = Product {
+    //     name: String::from("Book"),
+    //     price: 300.0,
+    //     in_stock: true,
+    //     category,
+    // };
 }
